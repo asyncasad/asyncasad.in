d384e8bb8aa137b87b956ee9a06d763dcd1421ec
Asadkhan Pathan <125233916+asyncasad@users.noreply.github.com>
2023-08-25 14:28:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Scalable Nextjs Architecture 💹
+
+Highly scalable and organised folder structure for your next Nextjs project. Includes svgr for svg icon component generation and tailwind utility functions.
+
+This is a great template to start a Nextjs project. Contains custom eslint and prettier configurations for consistent code.
---
+++ public/svgs/icons/iconcomponents/asyncasadlogo.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgAsyncasadlogo = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    data-name="Layer 2"
+    viewBox="0 0 1368 1368"
+    width={20}
+    height={20}
+    {...props}
+  >
+    <path
+      d="M854.39 939.58 770 1024 601.27 855.2l-256.4 256.4 168.75 168.75L426 1368 0 942l428.42-428.39Zm-425.15-256.4-256.4 256.4 84.38 84.42 256.4-256.4ZM1368 426 939.58 854.39l-426-426 87.65-87.65L770 509.52l256.4-256.4L857.66 84.37 942 0Zm-253.94-85.19-256.4 256.4L942 681.54l256.39-256.39Z"
+      className="asyncasadlogo_svg__cls-1"
+    />
+  </svg>
+);
+export default SvgAsyncasadlogo;
